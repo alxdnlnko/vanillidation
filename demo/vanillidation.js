95ff2783a5f2029a9d66bedf3cb943ef5f7fef12
Alex Danilenko <alxdnlnko@gmail.com>
2014-04-17 00:54:15 +1000

    showing error list

--- demo/vanillidation.js
+++ demo/vanillidation.js
@@ -36,7 +36,7 @@ module.exports = fn = {
     return expr.test(elem.value);
   },
   email: function(elem) {
-    return fn.regex(elem, new RegExp('^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'));
+    return fn.regex(elem, /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   }
 };
 
@@ -49,50 +49,78 @@ utils = require('./utils');
 
 Vanillidation = (function() {
   function Vanillidation(form, opts) {
-    var elem, _i, _len, _ref;
+    var elem, fields, _i, _len, _ref;
     this.form = form;
+    this.showFieldErrors = __bind(this.showFieldErrors, this);
     this.validateField = __bind(this.validateField, this);
     this.validators = require('./validators');
     this.messages = require('./messages');
-    this.rules = opts != null ? opts.rules : void 0;
-    this.fields = this.form.querySelectorAll('input[type="text"],input[type="password"],select,textarea');
-    _ref = this.fields;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      elem = _ref[_i];
+    this.rules = (_ref = opts != null ? opts.rules : void 0) != null ? _ref : {};
+    this.errors = {};
+    fields = this.form.querySelectorAll('input:not([type="submit"]),select,textarea');
+    for (_i = 0, _len = fields.length; _i < _len; _i++) {
+      elem = fields[_i];
       this.bindValidationEvents(elem, this.validateField);
     }
   }
 
   Vanillidation.prototype.validateField = function(elem) {
-    var name, opts, r, rules, _base, _base1, _i, _len, _ref, _results, _results1;
-    name = elem.getAttribute('name');
+    var name, opts, r, rules, _base, _base1, _i, _len, _ref, _ref1, _ref2;
+    name = elem.name;
     rules = (_ref = this.rules) != null ? _ref[name] : void 0;
     if (rules == null) {
       return;
     }
+    if (name in this.errors) {
+      delete this.errors[name];
+    }
     if (utils.isArray(rules)) {
-      _results = [];
       for (_i = 0, _len = rules.length; _i < _len; _i++) {
         r = rules[_i];
-        _results.push(console.log(typeof (_base = this.validators)[r] === "function" ? _base[r](elem) : void 0));
+        if (!(typeof (_base = this.validators)[r] === "function" ? _base[r](elem) : void 0)) {
+          this.errors[name] = (_ref1 = this.messages[r]) != null ? _ref1 : 'Invalid data.';
+          break;
+        }
       }
-      return _results;
     } else {
-      _results1 = [];
       for (r in rules) {
         opts = rules[r];
-        _results1.push(console.log(typeof (_base1 = this.validators)[r] === "function" ? _base1[r](elem, opts) : void 0));
+        if (!(typeof (_base1 = this.validators)[r] === "function" ? _base1[r](elem) : void 0)) {
+          this.errors[name] = (_ref2 = this.messages[r]) != null ? _ref2 : 'Invalid data.';
+          break;
+        }
       }
-      return _results1;
+    }
+    return this.showFieldErrors(elem);
+  };
+
+  Vanillidation.prototype.showFieldErrors = function(elem) {
+    var error, li, ul;
+    error = this.errors[elem.name];
+    if (error) {
+      elem.parentNode.classList.add('form-row--not-valid');
+      ul = elem.parentNode.getElementsByClassName('errorlist')[0];
+      if (!ul) {
+        ul = document.createElement('ul');
+        ul.className = 'errorlist';
+      }
+      li = document.createElement('li');
+      li.appendChild(document.createTextNode(error));
+      ul.appendChild(li);
+      return elem.parentNode.appendChild(ul);
     }
   };
 
   Vanillidation.prototype.bindValidationEvents = function(elem, handler) {
+    var self;
     elem.addEventListener('blur', function() {
       return handler(this);
     });
+    self = this;
     return elem.addEventListener('keyup', function() {
-      return handler(this);
+      if (this.name in self.errors) {
+        return handler(this);
+      }
     });
   };
 
--- src/vanillidation.coffee
+++ src/vanillidation.coffee
@@ -5,28 +5,54 @@ class Vanillidation
   constructor: (@form, opts) ->
     @validators = require './validators'
     @messages = require './messages'
+    @rules = opts?.rules ? {}
+    @errors = {}
 
-    @rules = opts?.rules
-    @fields = @form.querySelectorAll 'input[type="text"],input[type="password"],select,textarea'
-
-    for elem in @fields
+    fields = @form.querySelectorAll 'input:not([type="submit"]),select,textarea'
+    for elem in fields
       @bindValidationEvents elem, @validateField
 
   validateField: (elem) =>
-    name = elem.getAttribute 'name'
+    name = elem.name
     rules = @rules?[name]
     return if not rules?
 
+    delete @errors[name] if name of @errors
     if utils.isArray rules
       for r in rules
-        console.log @validators[r]?(elem)
+        if not @validators[r]?(elem)
+          @errors[name] = @messages[r] ? 'Invalid data.'
+          break
     else
       for r, opts of rules
-        console.log @validators[r]?(elem, opts)
+        if not @validators[r]?(elem)
+          @errors[name] = @messages[r] ? 'Invalid data.'
+          break
+
+    @showFieldErrors elem
+
+  showFieldErrors: (elem) =>
+    error = @errors[elem.name]
+
+    if error
+      elem.parentNode.classList.add 'form-row--not-valid'
+      ul = elem.parentNode.getElementsByClassName('errorlist')[0]
+      if not ul
+        ul = document.createElement 'ul'
+        ul.className = 'errorlist';
+
+      li = document.createElement 'li'
+      li.appendChild document.createTextNode error
+      ul.appendChild li
+
+      elem.parentNode.appendChild ul
 
   bindValidationEvents: (elem, handler) ->
     elem.addEventListener 'blur', -> handler @
-    elem.addEventListener 'keyup', -> handler @
+
+    self = @
+    elem.addEventListener 'keyup', ->
+      handler @ if @.name of self.errors
 
 
 module.exports = Vanillidation
